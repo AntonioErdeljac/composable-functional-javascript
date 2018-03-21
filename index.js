19968875102fd48bbb862a139ebe81cd8ede3026
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2018-03-22 00:00:33 +0100

    :hammer:

--- index.js
+++ index.js
@@ -54,6 +54,53 @@ const nextCharForNumberString = str =>
   .fold(i => String.fromCharCode(i));
 
 
-const result = applyDiscount('$5.00', '20%');
+// const result = applyDiscount('$5.00', '20%');
+
+
+const Right = x =>
+({
+  chain: f => f(x),
+  map: f => Right(f(x)),
+  fold: (f, g) => g(x),
+  inspect: () => `Right(${x})`
+});
+
+const Left = x =>
+({
+  map: f => Right(f(x)),
+  fold: (f, g) => g(x),
+  inspect: () => `Left(${x})`
+});
+
+const fromNullable = x =>
+ x != null ? Right(x) : Left(null)
+
+const fs = require('fs');
+
+const tryCatch = f => {
+  try {
+    return Right(f())
+  } catch(e) {
+    return Left(e);
+  }
+};
+
+// const getPort = () => {
+//   try {
+//     const str = fs.readFileSync('config.json');
+//     const config = JSON.parse(str);
+//     return config.port;
+//   } catch(e) {
+//     return 3000
+//   }
+// };
+
+const getPort = () =>
+  tryCatch(() => fs.readFileSync('config.json'))
+  .chain(c => tryCatch(() => JSON.parse(c)))
+  .fold(e => 3000,
+        c => c.port)
+
+const result = getPort();
 
 console.log(result);
