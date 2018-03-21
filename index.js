c3943a72af66e416a743893d55439fdb032faa3e
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2018-03-21 01:14:51 +0100

    practice :hammer:

--- index.js
+++ index.js
@@ -13,6 +13,38 @@ const Box = x =>
   inspect: f => `Box(${x})`
 })
 
+// const moneyToFloat = str =>
+//   parseFloat(str.replace(/\$/g, ''));
+
+
+const moneyToFloat = str =>
+    Box(str)
+    .map(s => s.replace(/\$/g, ''))
+     .map(r => parseFloat(r))
+
+// const percentToFloat = str => {
+//   const replaced = str.replace(/\$/g, '');
+//   const number = parseFloat(replaced);
+//   return number * 0.01;
+// };
+
+const percentToFloat = str =>
+     Box(str.replace(/\%/g, ''))
+     .map(replaced => parseFloat(replaced))
+     .map(number => number * 0.01)
+// const applyDiscount = (price, discount) => {
+//   const cost = moneyToFloat(price);
+//   const savings = percentToFloat(discount);
+//   return cost - cost * savings;
+// };
+
+const applyDiscount = (price, discount) =>
+     moneyToFloat(price)
+     .fold(cost =>
+       percentToFloat(discount)
+        .fold(savings =>
+          cost - cost * savings))
+
 const nextCharForNumberString = str =>
   Box(str)
   .map(s => s.trim())
@@ -22,6 +54,6 @@ const nextCharForNumberString = str =>
   .fold(i => String.fromCharCode(i));
 
 
-const result = nextCharForNumberString('  64 ');
+const result = applyDiscount('$5.00', '20%');
 
 console.log(result);
